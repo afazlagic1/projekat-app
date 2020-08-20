f1a7303f0d6e4737d9040e0804911542ffc5cf6d
afazlagic1 <afazlagic1@etf.unsa.ba>
2020-08-20 14:32:14 +0200

    User abstraktna klasa napravljena.

--- src/ba/unsa/etf/rpr/projekat/Main.java
+++ src/ba/unsa/etf/rpr/projekat/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package ba.unsa.etf.rpr.projekat;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Kindergarten");
+        primaryStage.setScene(new Scene(root, 700, 400));
         primaryStage.show();
     }
 
---
+++ src/ba/unsa/etf/rpr/projekat/User.java
@@ -0,0 +1,67 @@
+package ba.unsa.etf.rpr.projekat;
+
+import javafx.beans.property.SimpleStringProperty;
+
+public abstract class User {
+    private int id;
+    private SimpleStringProperty name;
+    private SimpleStringProperty surname;
+    private SimpleStringProperty email;
+    private SimpleStringProperty password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name.get();
+    }
+
+    public SimpleStringProperty nameProperty() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name.set(name);
+    }
+
+    public String getSurname() {
+        return surname.get();
+    }
+
+    public SimpleStringProperty surnameProperty() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname.set(surname);
+    }
+
+    public String getEmail() {
+        return email.get();
+    }
+
+    public SimpleStringProperty emailProperty() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email.set(email);
+    }
+
+    public String getPassword() {
+        return password.get();
+    }
+
+    public SimpleStringProperty passwordProperty() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password.set(password);
+    }
+}
