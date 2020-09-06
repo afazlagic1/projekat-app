aa0fe98a1798f5b0f75361bb92f03a9a62e03163
afazlagic1 <afazlagic1@etf.unsa.ba>
2020-09-06 14:36:47 +0200

    Ispravljen report.

--- src/ba/unsa/etf/rpr/projekat/PrintReport.java
+++ src/ba/unsa/etf/rpr/projekat/PrintReport.java
@@ -10,7 +10,7 @@ import java.util.HashMap;
 
 public class PrintReport extends JFrame {
     public void showReport(Connection conn) throws JRException {
-        String reportSrcFile = getClass().getResource("/reports/Kindergarten.jrxml").getFile();
+        String reportSrcFile = getClass().getResource("/reports/Child.jrxml").getFile();
         String reportsDir = getClass().getResource("/reports/").getFile();
 
         JasperReport jasperReport = JasperCompileManager.compileReport(reportSrcFile);
